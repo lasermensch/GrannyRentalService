3286908afe6bf56defd4761ed8986f5982a6472d
Erik Sundberg <sten.erik.sundberg@gmail.com>
2020-11-12 09:39:48 +0100

    Fixat så att seedingen i dbinitializer faktiskt fungerar.

---
+++ GrannyRentalService/Models/ViewModels/DeleteBookingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrannyRentalService.Models.ViewModels
+{
+    public class DeleteBookingViewModel
+    {
+        public Booking Booking { get; set; }
+        public Granny Granny { get; set; }
+        public User User { get; set; }
+    }
+}
